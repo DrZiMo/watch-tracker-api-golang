2fe0dc7b540ca19de4e34c9f9369546e253c9e03
DrZiMo <zuhaybhamar@gmail.com>
2026-04-02 20:36:09 +0300

    implement JWT token verification and enhance error handling in authentication middleware

--- middleware/authenticate.go
+++ middleware/authenticate.go
@@ -1,1 +1,37 @@
 package middleware
+
+import (
+	"net/http"
+
+	"github.com/DrZiMo/watch-tracker-api-golang/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate(c *gin.Context) {
+	token := c.Request.Header.Get("Authorization")
+
+	if token == "" {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"ok":      false,
+			"message": "Not authorized",
+		})
+
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"ok":      false,
+			"message": "Not authorized - invalid token",
+			"err":     err.Error(),
+		})
+
+		return
+	}
+
+	c.Set("userId", userId)
+
+	c.Next()
+}
--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"os"
 	"time"
 
@@ -14,4 +15,36 @@ func GenerateToke(userId int64) (string, error) {
 	})
 
 	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
+
+func VerifyToken(token string) (int64, error) {
+	parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (any, error) {
+		_, ok := t.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("Unexpected signing method")
+		}
+
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	tokenIsValid := parsedToken.Valid
+
+	if !tokenIsValid {
+		return 0, errors.New("Invalid token")
+	}
+
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+
+	if !ok {
+		return 0, errors.New("Invalid token claims")
+	}
+
+	userId := claims["userId"].(float64)
+
+	return int64(userId), nil
 }
